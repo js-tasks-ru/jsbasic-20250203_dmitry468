e8c6982ca0eb21af96cf11117e8c40e4660fc55b
DmitryKhakhin <dmitrykhakhin@gmail.com>
2025-03-01 16:34:39 +0300

    module 4 tasks 1-3

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement('ul');
+  let template = '';
+  for (let i = 0; i < friends.length; i += 1) {
+    template += `<li>${friends[i].firstName} ${friends[i].lastName}</li>`
+  }
+  ul.innerHTML = template;
+  return ul
 }
--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,9 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  for (let i=0; i < table.rows.length; i++) {
+    for (let k=0; k < table.rows[i].cells.length; k++) {
+      if (i == k) {
+        table.rows[i].cells[k].style.backgroundColor = 'red';
+      }
+    }
+  }
 }
