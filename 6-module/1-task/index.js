d943074ea1e611d59a81b9126df7de28defb00e4
DmitryKhakhin <dmitrykhakhin@gmail.com>
2025-03-09 20:26:28 +0300

    module 6 tasks 1-3

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,49 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = this.render();
   }
+
+  template(){
+    return `
+    <table>
+        <thead>
+          <tr>
+            <th>Имя</th>
+            <th>Возраст</th>
+            <th>Зарплата</th>
+            <th>Город</th>
+            <th></th>
+        </tr>
+        </thead>
+      <tbody>
+        ${this.rows.map(row => `
+        <tr>
+          <td>${row.name}</td>
+          <td>${row.age}</td>
+          <td>${row.salary}</td>
+          <td>${row.city}</td>
+          <td><button class="remove-button">X</button></td>
+        </tr>
+      `).join('')}
+      </tbody>
+  </table>
+  `;
+  }
+
+  onXClick = (event) => {
+      if (event.target.classList.contains('remove-button')) {
+        event.target.closest('tr').remove();
+      }
+    }
+
+  render() {
+    this.elem = document.createElement('table');
+    this.elem.innerHTML = this.template()
+    this.elem.addEventListener('click', this.onXClick);
+
+    return this.elem;
+  }
+
 }
